ba1723bd9852ca803add12aba45c2400a6e1c2b6
Lucas.Valentim <lucas.valentim.costa@gmail.com>
2024-01-29 17:13:42 -0300

    :recycle: refactor: add tratamento de exception

---
+++ src/main/java/br/com/advantageshoppingautomation/utils/AutomationException.java
@@ -0,0 +1,8 @@
+package br.com.advantageshoppingautomation.utils;
+
+public class AutomationException extends RuntimeException {
+
+    public AutomationException(String message) {
+        super(message);
+    }
+}
